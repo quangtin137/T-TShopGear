a8f17d9dd81bca1398c27fe9994a80fb6f39d281
Quoc Thaii <qthai0927@gmail.com>
2025-06-28 20:44:33 +0700

    [Add something]

---
+++ VanQuangTin_2280603267_Lab04/Controllers/PaymentController.cs
@@ -0,0 +1,59 @@
+﻿using GearShop.Models;
+using GearShop.Services.Momo;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using VanQuangTin_2280603267_Lab04.Models;
+
+namespace GearShop.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IMomoService _momoService;
+        private readonly ApplicationDbContext _context;
+
+        public PaymentController(IMomoService momoService, ApplicationDbContext context)
+        {
+            _momoService = momoService;
+            _context = context;
+        }
+
+        [HttpPost]
+      
+        public async Task<IActionResult> CreatePaymentUrl(OrderInfoModel model)
+        {
+            var response = await _momoService.CreatePaymentAsync(model);
+            return Redirect(response.PayUrl);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> PaymentCallBack(MomoInfoModel model)
+        {
+            var response = _momoService.PaymentExecuteAsync(HttpContext.Request.Query);
+            var requestQuery = HttpContext.Request.Query;
+            //if (requestQuery["resultCode"] != "0")
+            //{
+            //    var newMomoInsert = new MomoInfoModel
+            //    {
+            //        OrderId = requestQuery["orderId"],
+            //        FullName = User.FindFirstValue(ClaimTypes.Email),
+            //        Amount = decimal.Parse(requestQuery["amount"]),
+            //        OrderInfo = requestQuery["orderInfo"],
+            //        DatePaid = DateTime.Now
+            //    };
+
+            //    _context.Add(newMomoInsert);
+            //    await _context.SaveChangesAsync();
+            //}
+            //else
+            //{
+            //    TempData["success"] = "Giao dịch không thành công.";
+            //    return RedirectToAction("OrderCompleted", "ShoppingCart");
+            //}
+
+            ////var checkoutResult = await Checkout(requestQuery["orderId"]);
+            return View(response);
+        }
+
+    }
+}
---
+++ VanQuangTin_2280603267_Lab04/Models/MomoInfoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GearShop.Models
+{
+    public class MomoInfoModel
+    {
+        public string OrderId { get; set; }
+        public string FullName { get; set; }
+        public decimal Amount { get; set; }
+        public string OrderInfo { get; set; }
+        public DateTime DatePaid { get; set; }
+    }
+
+}
