a62b412cacc4a20947fe4f6194ace894649e7214
quangtin137 <quangtin13072004@gmail.com>
2025-07-01 12:27:03 +0700

    [add] HuongDan

---
+++ VanQuangTin_2280603267_Lab04/Controllers/HuongDanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GearShop.Controllers
+{
+    public class HuongDanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
